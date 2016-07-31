cfa654b88fce5ed4219f6ad8b070502bdb20c3a8
Jack <JackCDK@users.noreply.github.com>
2016-07-31 05:29:53 +0100

    Add files via upload

---
+++ versions/python3/bbomb_1.0.6_p3.py
@@ -0,0 +1,53 @@
+#browser bomb by JackCDK
+#adding your name to the credits dosn't make you a programmer.
+import webbrowser
+import time
+from itertools import repeat
+
+#variables
+hlink = "http://"
+websi = "www.google.com" 
+num = 1
+
+t1 = "===========[python3 edition]========="
+t2 = "   ___         ___            _      "
+t3 = "  | _ )  ___  | _ ) ___ _ __ | |__   "
+t4 = "  | _ \ |___| | _ \/ _ \ '  \| '_ \  "
+t5 = "  |___/       |___/\___/_|_|_|_.__/  "
+t6 = "                                     "
+t7 = "        Browser Bomb By JackCDK      "
+ta = "               [V1.0.6]              "
+tb = "        [www.Jackcdk.comxa.com]      "
+t8 = "====================================="
+t9 =""
+
+print(t1)
+print(t2)
+print(t3)
+print(t4)
+print(t5)
+print(t6)
+print(t7)
+print(ta)
+print(tb)
+print(t8)
+print(t9)
+
+
+websi = input("[?] What site do you want to use?:")
+while True:
+    try:
+        num = int(input("[?]How many times should your site open?: "))
+    except ValueError:
+        print("[-] Sorry, I didn't understand that.")
+        #try again
+        continue
+    else:
+        #num successfully parsed!
+        #exit the loop.
+        break
+time.sleep(2)
+for word in ['spaming'] * num:
+        webbrowser.open(hlink+websi)
+        time.sleep(0.4)
+
